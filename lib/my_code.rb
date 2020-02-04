314991d56d411868fe7b1c76d5dad01d37209c48
Yahya  Gok <ygok17@hotmail.com>
2020-02-04 22:47:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -52,11 +52,12 @@ end
  
  def reduce_to_all_true(source_array)
   index = 0 
-  has_false = false
+  at_least_one_is_false = false
   
    while index < source_array.length do 
-     if !source_array[index] 
-       has_false = true 
+     if source_array[index] 
+        true  
+       
      end
    end 
    
