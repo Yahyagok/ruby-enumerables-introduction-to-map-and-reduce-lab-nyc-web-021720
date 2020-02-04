87fe201b0cd526b2786649f1b3ff7731f79d9161
Yahya  Gok <ygok17@hotmail.com>
2020-02-04 21:40:17 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,65 @@
 # My Code here....
+require 'pry'
+def map_to_negativize(source_array)
+  index = 0 
+  while index < source_array.length 
+  source_array[index] *= -1
+  index +=1
+end 
+source_array
+end
+
+ def map_to_no_change(source_array)
+   source_array
+   
+ end 
+ 
+ 
+def map_to_double(source_array)
+  index = 0 
+  while index < source_array.length 
+  source_array[index] *= 2
+  index +=1
+end 
+source_array
+end 
+
+def map_to_square(source_array)
+   index = 0 
+  while index < source_array.length 
+  source_array[index] = source_array[index]**2
+  index +=1
+end 
+  source_array
+end 
+
+
+ def reduce_to_total(source_array, starting_point)
+  
+  index =0 
+  while index < source_array.length 
+  
+   source_array[index] += starting_point
+   source_array[index] += source_array[index]
+   
+   
+   
+   
+   
+   index +=1
+ end 
+ 
+   source_array
+   
+   
+ end 
+
+
+
+
+
+
+
+
+
+
